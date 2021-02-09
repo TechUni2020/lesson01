6cdf52e337b9afab18b54ec6c5fd88ba1d6f5d1c
Chisa-Tojo <75416663+Chisa-Tojo@users.noreply.github.com>
2021-02-09 16:06:58 +0900

    コマンドライン引数で指定した金額を100円玉と10円玉と1円玉だけで、できるだけ少ない枚数で支払いたい。金額を入力するとそれぞれの枚数を計算して表示するプログラムを作成せよ

---
+++ 2-5/main.js
@@ -0,0 +1,7 @@
+const money = Number(process.argv[2]);
+
+const hundred = Math.floor(money / 100);
+const ten = Math.floor(money % 100 / 10);
+const one = Math.floor (money % 100 % 10 / 1);
+
+console.log("100円玉" + (hundred) + "枚,", "10円玉" + (ten) + "枚," ,"1円玉" + (one) + "枚");
