3ebdc2e0a003c45ad538bc998d2d8c10bbeca210
Chisa Tojo <75416663+Chisa-Tojo@users.noreply.github.com>
2021-02-10 02:13:56 +0900

    Update main.js

--- 2-5/main.js
+++ 2-5/main.js
@@ -4,4 +4,4 @@ const hundred = Math.floor(money / 100);
 const ten = Math.floor(money % 100 / 10);
 const one = Math.floor (money % 100 % 10 / 1);
 
-console.log("100円玉" + (hundred) + "枚,", "10円玉" + (ten) + "枚," ,"1円玉" + (one) + "枚");
+console.log(`100円玉${hundred}枚, 10円玉${ten}枚, 1円玉${one}枚`);
