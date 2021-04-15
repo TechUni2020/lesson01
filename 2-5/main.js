83d0c74b6e195903fcb9b85007903bc2077923b4
sawa-4 <30shi4s@gmail.com>
2021-04-15 22:58:08 +0900

    2-5 小銭の枚数を出す

---
+++ 2-5/main.js
@@ -0,0 +1,13 @@
+let n = Number(process.argv[2]);
+let coin = 100;
+let text = "";
+while(coin >= 1){
+    let a = Math.floor(n/ coin);
+    text  += (coin + "円玉" + a + "枚");
+    if (coin > 1){
+        text += ", ";
+    }
+    n -= coin * a;
+    coin /= 10;
+}
+console.log(text);
