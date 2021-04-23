9f3a06b3581b0c5c8baa934f7045160e5ede8ee4
LAPTOP-TI09ET7L\Owner <shotabook1121@outlook.jp>
2021-04-23 20:05:54 +0900

    コマンドライン引数で渡した月が季節で帰ってくるプログラムをswitch文を使って書こう

---
+++ 2-4/main.js
@@ -0,0 +1,43 @@
+const season=Number(process.argv[2]);
+
+switch (season) {
+  case 11:
+    console.log("冬");
+    break;
+  case 12:
+    console.log("冬");
+    break;
+  case 1:
+    console.log("冬");
+    break; 
+  case 10:
+    console.log("秋");
+    break;
+  case 9:
+    console.log("秋");
+    break;
+  case 8:
+    console.log("秋");
+    break;   
+  case 7:
+    console.log("夏");
+    break;
+  case 6: 
+   console.log("夏");
+   break;
+  case 5:
+    console.log("夏");
+    break;
+  case 4:
+    console.log("春");
+    break;
+  case 3:
+    console.log("春");
+    break;
+  case 2:
+    console.log("春");
+    break;
+        
+
+ 
+}
