b854232531978af61d53ee3921767a79c8c7181b
tainona <tainona0119@outlook.jp>
2021-04-24 00:40:19 +0900

    3-3,3-4

---
+++ 3-2/main.py
@@ -0,0 +1,18 @@
+a = list(map(int,input().split()))
+b = []
+n = 0
+s = a[1]
+for i in range(a[0]):
+    for j in range(a[1]):
+        if i%2 ==0 and j%2 == 0:
+            b.append('*')
+        elif i%2 ==0 and j%2 == 1:
+            b.append('-')
+        elif i%2 == 1 and j%2 ==0:
+            b.append('-')
+        elif i%2 == 1 and j%2 ==1:
+            b.append('*')
+
+for i in range(a[0]):
+    print(*b[n:n+s:1])
+    n +=s
---
+++ 3-3/main.py
@@ -0,0 +1,12 @@
+a = int(input())
+b = []
+for i in range(1,a+1):
+    for j in range(a-i):
+        b.append(' ')
+    for k in range(i*2-1):
+        b.append('*')
+n = 0
+for i in range(a):
+    print(*b[n:n+a:1])
+    n += a
+    a+= 1
