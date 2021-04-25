c4870dd8143449bdff32d59621771f6806d8cb8c
yushi127 <80163799+yushi127@users.noreply.github.com>
2021-04-25 20:50:59 +0900

    Update main.js

--- 4-3/main.js
+++ 4-3/main.js
@@ -3,3 +3,26 @@ const errorLogs = [
   "Error 3:Invalid Exception BBBB - 2020/01/01/02:00",
   "Error 22:Invalid Exception CCCCC - 2020/01/01/03:00"
 ]
+// for (let i = 0; i < errorLogs.length; i++) {
+//   const result = errorLogs[i].match(/:(.*) -/);
+//   if (result != null) {
+//       console.log(result[i]); 
+//   }
+//   result.length = 0;
+// }
+
+const before = ':';
+const after =  '-';
+
+for (let i = 0; i < errorLogs.length; i++) {
+  const beforeIdx = errorLogs[i].indexOf(before);
+  // strの中のafterを後ろから検索
+  const afterIdx = errorLogs[i].lastIndexOf(after);
+
+
+  if (beforeIdx >= 0 && afterIdx >= 0) {
+      // 両方とも見つかったら中身を取り出す
+      const result = errorLogs[i].substring(beforeIdx + before.length, afterIdx);
+      console.log(result); 
+  }  
+}
