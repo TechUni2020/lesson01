d49f536e8758bffb4295da75050ad8b8333f2175
sawa-4 <30shi4s@gmail.com>
2021-04-28 22:54:08 +0900

    ２‐５変数展開にしてみたやつ

--- 2-5/main.js
+++ 2-5/main.js
@@ -1,11 +1,24 @@
+// let n = Number(process.argv[2]);
+// let coin = 100;
+// let text = "";
+// while(coin >= 1){
+//     let a = Math.floor(n/ coin);
+//     text  += (coin + "円玉" + a + "枚");
+//     if (coin > 1){
+//         text += ", ";
+//     }
+//     n -= coin * a;
+//     coin /= 10;
+// }
+// console.log(text);
 let n = Number(process.argv[2]);
 let coin = 100;
 let text = "";
 while(coin >= 1){
     let a = Math.floor(n/ coin);
-    text  += (coin + "円玉" + a + "枚");
+    text  += `${coin}円玉 ${a}枚`;
     if (coin > 1){
-        text += ", ";
+        text += ", "
     }
     n -= coin * a;
     coin /= 10;
