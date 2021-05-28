90df00ece1b16f0c485ceac375912e5be7688a5b
KaiKohlman <kaikohlman@gmail.com>
2021-05-28 16:23:05 +0900

    時刻のうち n 時の部分が日をまたいで 25 時となってしまった場合は 1 時とするように 0 時から 23 時までの 24 時間表記として正しい数値に修正するプログラムを作成してください。

--- 2-3/main.js
+++ 2-3/main.js
@@ -1,8 +1,10 @@
 const arg1=process.argv[2];
 if(arg1<24){
   console.log(arg1);
-}else if(arg1>24){
+}else if(arg1>=24){
   console.log(arg1-24);
+}else if(arg124){
+  console.log("0");
 }
 
 console.log("追記：% 24でもできるのか、ひえ〜");
