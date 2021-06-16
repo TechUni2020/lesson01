a8deee50c8ddec7fdeec9ef24a8df8b09dbbb8cc
Shimizu-Yuuki <yuuki23112@gmail.com>
2021-06-16 19:50:34 +0900

    Update main.js

---
+++ 2-4/main.js
@@ -0,0 +1,24 @@
+const mon=process.argv[2];
+
+switch(mon){
+    case '2':
+    case '3':
+    case '4':
+    console.log('春'); break;
+
+    case '5':
+    case '6':
+    case '7':
+    console.log('夏'); break;
+
+    case '8':
+    case '9':
+    case '10':
+    console.log('秋'); break;
+
+    case '11':
+    case '12':
+    case '1':
+    console.log('冬'); break;
+    
+}
