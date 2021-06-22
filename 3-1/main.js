91991c0471179f49bdcaad7e93a71c3c528321db
Shimizu-Yuuki <yuuki23112@gmail.com>
2021-06-23 01:45:20 +0900

    Update main.js

--- 3-1/main.js
+++ 3-1/main.js
@@ -1,12 +1,12 @@
-const num=[1,2,3,4,5,6,7,8,9];
 
-for(let i=0;i<num.length;i++){
+
+for(let i=1;i<9;i++){//配列なんかいらなかったことに気づきました（笑）
 
   var array=[];
 
   for(let j=1;j<=9;j++){
 
-    let result=num[i]*j;
+    let result=i*j;
     array.push(result);
     
   }
