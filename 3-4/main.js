9cc3a7c3d87fa2bee890f1b89ad5ca4382d93b8a
Shimizu-Yuuki <yuuki23112@gmail.com>
2021-06-23 02:26:59 +0900

    Update main.js

--- 3-4/main.js
+++ 3-4/main.js
@@ -3,13 +3,15 @@ const arg2=process.argv[3];
 
 const array1=arg1.split("");
   array1.reverse("");
-  let beforenum1= array1.join("");
-  let num = Number(beforenum1);
-
+  const beforenum1= array1.join("");
+  //let num = Number(beforenum1);
+//console.log(beforenum1);
 const array2=arg2.split("");
   array2.reverse("");
-  let beforenum2=array2.join("");
-  let num1=Number(beforenum2);
+  const beforenum2=array2.join("");
+  //let num1=Number(beforenum2);
 
-const result=num+num1;
+const result=Number(beforenum1)+Number(beforenum2);
 console.log(result);
+
+//これではだめかな？
