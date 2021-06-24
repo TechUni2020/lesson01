ebb72e5655678787f3971c0ad49e6e1d7b063b6b
Shimizu-Yuuki <yuuki23112@gmail.com>
2021-06-25 00:36:18 +0900

    Update main.js

--- 4-3/main.js
+++ 4-3/main.js
@@ -3,3 +3,14 @@ const errorLogs = [
   "Error 3:Invalid Exception BBBB - 2020/01/01/02:00",
   "Error 22:Invalid Exception CCCCC - 2020/01/01/03:00"
 ]
+const colon=":";
+const hyphen="-";
+for(let i=0;i<errorLogs.length;i++){
+  const before=errorLogs[i].indexOf(colon);
+  const after=errorLogs[i].lastIndexOf(hyphen);
+
+if(before>=0&&after>=0){
+  const result=errorLogs[i].substring(before+colon.length,after);
+console.log(result);
+}
+}
