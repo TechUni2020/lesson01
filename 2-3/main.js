c4eb7bc78494c49ce229851264fa57e782e08db9
Keiji-morita <keijim3116@gmail.com>
2021-08-09 18:35:29 +0900

    2-3 解き直しです

--- 2-3/main.js
+++ 2-3/main.js
@@ -2,4 +2,6 @@ const number = process.argv[2];
 
 if (number => 24){
   console.log(number % 24);
+}else if(number <= 23){
+  console.log(number);
 };
---
+++ 2-4/main.js
@@ -0,0 +1,20 @@
+const month = process.argv[2];
+switch(month){
+  case rank >= 2 && rank <= 4:
+    console.log("春");
+   break;
+  
+  case rank >= 5 && rank <= 7:
+     console.log("夏");
+   break;
+
+  case rank >= 8 && rank <= 10:
+     console.log("秋");
+   break;
+
+  case rank >= 11 && rank <= 12:
+  case rank = 1:
+     console.log("冬");
+   break;
+
+};
