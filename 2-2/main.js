bc9a5f7a4c97d809ca7a68b10edf37b1170695a2
Ian Syu <dev.iansyu@gmail.com>
2021-09-13 12:32:33 +0900

    コマンドライン引数で指定された整数を半径とした円の面積を求めよ。ただし円周率は3.14とする

---
+++ 2-2/main.js
@@ -0,0 +1,5 @@
+// Calculate the area of a circle with radius.
+const r = process.argv[2];
+// Use Math.pow(a, b); where a is the value and b is the exponent.
+const area = 3.14 * Math.pow(r, 2);
+console.log(`${area}`);
