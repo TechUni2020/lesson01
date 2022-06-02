faeff52ead3c2baf953819a2fc3ab3912be7ef9e
はやと <104907508+yonehaya@users.noreply.github.com>
2022-06-02 13:33:40 +0900

    2-4

---
+++ 2-4/main.js
@@ -0,0 +1,16 @@
+const season =process.argv[2];
+
+switch(season){
+  case "2": case "3": case "4":
+    console.log("春");
+    break;
+  case "5": case "6": case "7":
+    console.log("夏");
+    break;
+  case "8": case "9": case "10":
+    console.log("秋");
+    break;
+  case "11": case "12": case "1":
+    console.log("冬");
+    break;       
+}
